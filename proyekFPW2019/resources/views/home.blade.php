b6772aab2a9fe8aeb811994c21e557141fbd1308
Cosmas Yustianus Billiarta <billiarta.gondokusuma@gmail.com>
2019-11-13 23:23:17 +0700

    buat navbar & kasi bootstrap

---
+++ proyekFPW2019/resources/views/home.blade.php
@@ -0,0 +1,2 @@
+@extends('layouts.page')
+@section('judul_page',"Home | Kaskus")
---
+++ proyekFPW2019/resources/views/layouts/navbar.blade.php
@@ -0,0 +1,4 @@
+<nav class="navbar bg-primary">
+    <div name="logo_kiri"><a href="#" class="navbar-brand text-white">KASKUS</a></div>
+    <div name="kanan"><a href="#" class="navbar-brand text-white">KASKUS</a></div>
+</nav>
