d27cee2c61334346331b6d5ed0834b5528dbac81
chrysmanyoel <50228438+chrysmanyoel@users.noreply.github.com>
2019-12-01 16:43:31 +0700

    ubaah jabatan

--- proyekFPW2019/app/Http/Controllers/GuestController.php
+++ proyekFPW2019/app/Http/Controllers/GuestController.php
@@ -68,7 +68,7 @@ class GuestController extends BaseController
 				$provinsi_user	= "";
 				$ctr_post		= 0;
 				$join_date		= date('Y-m-d H:i:s');
-				$jabatan_user	= "";
+				$jabatan_user	= "newbie";
 				$dbuser->insertdata($username,$password,$email,$nama,$nomor,$tgl_lahir_user,$jk_user,$bio_profil,$alamat_user,
 				$negara_user,$provinsi_user,$ctr_post,$join_date,$jabatan_user); 
 				Session::flash("berhasil","Akun anda berhasil didaftarkan");
@@ -101,6 +101,11 @@ class GuestController extends BaseController
 	}
 
 	public function createpost(Request $request){
+
+		$request->validate([
+			"txtrobot"=>"required",
+		]);
+		
 		if($request->btnpost)
 		{
 			$dbmodelpost 		= new modelpost();
@@ -120,6 +125,5 @@ class GuestController extends BaseController
 		}else{
 			return view("createpost");
 		}
-				
 	}
 }
